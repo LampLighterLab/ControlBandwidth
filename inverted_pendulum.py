a82d52af51d9601971ae5f1a26a180ff4f0416ff
Steve Heim <heim.steve@gmail.com>
2025-09-01 14:32:40 -0400

    some refactoring, still bleeding a bit of energy

--- inverted_pendulum.py
+++ inverted_pendulum.py
@@ -7,29 +7,23 @@ class InvertedPendulum:
     # params is a dict containing keys "mass", "length", "gravity"
     def __init__(self, params, initial_state, sim_timestep=0.01, control_timestep=0.1):
         self.params = params
-        self.angular_pos = initial_state[
-            0
-        ]  # 0 is pointing straight up, positive clockwise
-        self.angular_vel = initial_state[1]  # Positive velocity is clockwise
+        self.angular_pos = initial_state[0]
+        # ! 0 is pointing straight down (passively stable equilibrium point), positive follows RHR!
+        self.angular_vel = initial_state[1]
         self.sim_timestep = sim_timestep
         self.control_timestep = control_timestep
 
     # `T_app` is the applied torque on the pendulum
-    def get_next_state(self, T_app, state):
-        t = 0
-        m = self.params["mass"]
-        l = self.params["length"]
-        g = self.params["gravity"]
-        I = m * l * l
-        while t <= self.control_timestep - 0.5 * self.sim_timestep:
-            state = np.add(
-                state,
-                np.multiply(
-                    self.sim_timestep,
-                    [state[1], -(g * np.sin(state[0])) / l + T_app / I],
-                ),
+    def get_next_state(self, torque, state):
+        mass = self.params["mass"]
+        length = self.params["length"]
+        gravity = self.params["gravity"]
+        inertia = mass * length * length
+        timestep = self.sim_timestep
+        for _ in range(int(self.control_timestep // self.sim_timestep)):
+            state += timestep * np.array(
+                [state[1], -(gravity * np.sin(state[0])) / length + torque / inertia]
             )
-            t += self.sim_timestep
         return state
 
     def get_potential_energy(self, state):
@@ -64,7 +58,7 @@ class InvertedPendulum:
             self.params["mass"]
             * self.params["gravity"]
             * self.params["length"]
-            * (np.cos(pos) - 1)
+            * (1 - np.cos(pos))
         )
         return np.array([np.cos(pos), np.sin(pos), vel, abs(vel), KE, PE, 1])
 
--- test_sandbox.py
+++ test_sandbox.py
@@ -12,8 +12,8 @@ def test_pendulum():
 
     # initial_state = np.array([0.5 * np.pi, 0.1])
     params = {"mass": 1, "length": 1, "gravity": 1}
-    initial_state = np.array([0.0, -1.0])
-    sim_timestep = 0.01
+    initial_state = np.array([np.pi, 0.01])
+    sim_timestep = 0.0001
     control_timestep = 0.01
     env = InvertedPendulum(
         params=params,
@@ -28,6 +28,8 @@ def test_pendulum():
     for i in range(int(sim_time // sim_timestep)):
         state_traj[:, i + 1] = env.get_next_state(0, state_traj[:, i])
     state_traj[0, :] = np.mod(state_traj[0], 2 * np.pi)
+
+    plt.figure(1)
     plt.scatter(
         state_traj[0, :], state_traj[1, :], c=range(int(sim_time // sim_timestep) + 1)
     )
@@ -35,18 +37,22 @@ def test_pendulum():
     plt.colorbar(label="Timestep")
     plt.xlabel("pos")
     plt.ylabel("vel")
-    plt.show()
+    # plt.show()
 
+    plt.figure(2)
     time_traj = np.array(range(int(sim_time // sim_timestep) + 1)) * sim_timestep
     potential_energy = env.get_potential_energy(state_traj)
     kinetic_energy = env.get_kinetic_energy(state_traj)
 
     plt.plot(time_traj, potential_energy)
     plt.plot(time_traj, kinetic_energy)
-    plt.plot(time_traj, potential_energy + kinetic_energy)
     plt.xlabel("time [s]")
     plt.ylabel("energy [j]")
-    plt.legend(["potential", "kinetic", "total"])
+    plt.legend(["potential", "kinetic"])
+    plt.figure(3)
+    plt.plot(time_traj, potential_energy + kinetic_energy)
+    plt.ylabel("totla energy [j]")
+    plt.xlabel("time [s]")
     plt.show()
 
 
